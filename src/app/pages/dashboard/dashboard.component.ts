f0d91d8edcd72b79210ea661656a351e032b6a22
AMoharib <AbdelRahman.Moharib@KAEgypt.local>
2019-10-07 18:29:05 +0200

    Moinday

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,13 @@
-import {Component, OnDestroy } from '@angular/core';
-import { NbThemeService } from '@nebular/theme';
-import { takeWhile } from 'rxjs/operators' ;
-import { SolarData } from '../../@core/data/solar';
-import { DomSanitizer, SafeResourceUrl ,  SafeUrl} from '@angular/platform-browser';
+import { Component, OnDestroy } from "@angular/core";
+import { NbThemeService } from "@nebular/theme";
+import { SolarData } from "../../@core/data/solar";
+import { DataService } from "../../Services/data.service";
+
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl
+} from "@angular/platform-browser";
 
 interface CardSettings {
   title: string;
@@ -11,102 +16,56 @@ interface CardSettings {
 }
 
 @Component({
-  selector: 'ngx-dashboard',
-  styleUrls: ['./dashboard.component.scss'],
-  templateUrl: './dashboard.component.html',
+  selector: "ngx-dashboard",
+  styleUrls: ["./dashboard.component.scss"],
+  templateUrl: "./dashboard.component.html"
 })
 export class DashboardComponent implements OnDestroy {
+  tusCards: string;
 
-  private alive = true;
+  // url for iframe
+  private MapView: SafeUrl;
+  private TelemetryView: SafeResourceUrl;
 
-  solarValue: number;
-  lightCard: CardSettings = {
-    title: 'Light',
-    iconClass: 'nb-lightbulb',
-    type: 'primary',
-  };
-  rollerShadesCard: CardSettings = {
-    title: 'Roller Shades',
-    iconClass: 'nb-roller-shades',
-    type: 'success',
-  };
-  wirelessAudioCard: CardSettings = {
-    title: 'Wireless Audio',
-    iconClass: 'nb-audio',
-    type: 'info',
-  };
-  coffeeMakerCard: CardSettings = {
-    title: 'Coffee Maker',
-    iconClass: 'nb-coffee-maker',
-    type: 'warning',
-  };
-
-  statusCards: string;
-
-  commonStatusCardsSet: CardSettings[] = [
-    this.lightCard,
-    this.rollerShadesCard,
-    this.wirelessAudioCard,
-    this.coffeeMakerCard,
-  ];
-
-  statusCardsByThemes: {
-    default: CardSettings[];
-    cosmic: CardSettings[];
-    corporate: CardSettings[];
-    dark: CardSettings[];
-  } = {
-    default: this.commonStatusCardsSet,
-    cosmic: this.commonStatusCardsSet,
-    corporate: [
-      {
-        ...this.lightCard,
-        type: 'warning',
-      },
-      {
-        ...this.rollerShadesCard,
-        type: 'primary',
-      },
-      {
-        ...this.wirelessAudioCard,
-        type: 'danger',
-      },
-      {
-        ...this.coffeeMakerCard,
-        type: 'info',
-      },
-    ],
-    dark: this.commonStatusCardsSet,
-  };
- // url for iframe
- private MapView:SafeUrl;
- private TelemetryView:SafeResourceUrl;
-
- // toggle iframe 
- private download:boolean = false;
-
-
-  constructor(private themeService: NbThemeService,
-              private solarService: SolarData,
-              private sanitizer: DomSanitizer) {
-    this.themeService.getJsTheme()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(theme => {
-        this.statusCards = this.statusCardsByThemes[theme.name];
-    });
-    this.MapView = this.sanitizer.bypassSecurityTrustResourceUrl("https://demo.thingsboard.io/dashboards/1f9828d0-058e-11e7-87f7-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0&source=realtimeIotDashboards")   
-    this.TelemetryView = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.yahoo.com")
-    this.solarService.getSolarData()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe((data) => {
-        this.solarValue = data;
-      });
+  // toggle iframe
+  private download: boolean = false;
+  LowerDashboardHieght: any;
+  UpperDashboardHieght: any;
+  constructor(
+    private themeService: NbThemeService,
+    private DataService: DataService,
+    private solarService: SolarData,
+    private sanitizer: DomSanitizer
+  ) {
+    this.LowerDashboardHieght = "200";
+    this.UpperDashboardHieght = "650";
+    this.MapView = this.sanitizer.bypassSecurityTrustResourceUrl(
+      "https://demo.thingsboard.io/dashboards/1f9828d0-058e-11e7-87f7-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0&source=realtimeIotDashboards"
+    );
+    this.TelemetryView = this.sanitizer.bypassSecurityTrustResourceUrl(
+      "https://demo.thingsboard.io/dashboard/3a1026e0-83f6-11e7-b56d-c7f326cba909?publicId=322a2330-7c36-11e7-835d-c7f326cba909"
+    );
   }
-  
-  // loc :any="https://demo.thingsboard.io/dashboards/1f9828d0-058e-11e7-87f7-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0&source=realtimeIotDashboards"
-  // f:any=document.getElementById['hello'].src= this.loc;
-  
-  ngOnDestroy() {
-    this.alive = false;
+
+  hideElement = true;
+  onclick(s) {
+    this.hideElement = !this.hideElement;
+
+    this.DataService.notify();
+    if (this.LowerDashboardHieght == "200") {
+      this.TelemetryView = this.sanitizer.bypassSecurityTrustResourceUrl(
+        "https://demo.thingsboard.io/dashboard/3d0bf910-ee09-11e6-b619-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0"
+      );
+      console.log("ok");
+
+      this.LowerDashboardHieght = "850";
+    } else {
+      this.TelemetryView = this.sanitizer.bypassSecurityTrustResourceUrl(
+        "https://demo.thingsboard.io/dashboard/3a1026e0-83f6-11e7-b56d-c7f326cba909?publicId=322a2330-7c36-11e7-835d-c7f326cba909"
+      );
+
+      this.LowerDashboardHieght = "200";
+    }
   }
+  ngOnDestroy() {}
 }
--- src/app/pages/e-commerce/e-commerce.component.ts
+++ src/app/pages/e-commerce/e-commerce.component.ts
@@ -1,4 +1,5 @@
 import { Component ,ViewChild,ElementRef} from '@angular/core';
+import { DomSanitizer, SafeResourceUrl ,  SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'ngx-ecommerce',
@@ -6,4 +7,21 @@ import { Component ,ViewChild,ElementRef} from '@angular/core';
 })
 export class ECommerceComponent {
   
+
+
+   // url for iframe
+ private MapView:SafeUrl;
+ private TelemetryView:SafeResourceUrl;
+
+ // toggle iframe 
+ private download:boolean = false;
+
+
+  constructor(
+              private sanitizer: DomSanitizer) {
+    
+    this.MapView = this.sanitizer.bypassSecurityTrustResourceUrl("https://demo.thingsboard.io/dashboards/1f9828d0-058e-11e7-87f7-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0&source=realtimeIotDashboards")   
+    this.TelemetryView = this.sanitizer.bypassSecurityTrustResourceUrl("https://demo.thingsboard.io/dashboards/1f9828d0-058e-11e7-87f7-bb0136cc33d0?publicId=963ab470-34c9-11e7-a7ce-bb0136cc33d0&source=realtimeIotDashboards")
+   
+  }
 }
